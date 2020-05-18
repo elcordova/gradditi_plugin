a0ed1fd8423d6fe832526b086b26e5ea03f515c2
elcordova <erick.lenin.03@gmail.com>
2020-05-18 17:19:57 -0500

    madded: manejo de lobrerias

--- functions.php
+++ functions.php
@@ -5,3 +5,11 @@ function init_template() {
 }
 
 add_action('after_setup_theme', 'init_template');
+
+function assets() {
+  wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css', '', '4.5.0', 'all');
+  wp_register_style('montserrat','https://fonts.googleapis.com/css2?family=Montserrat&display=swap','','1.0', 'all');
+  wp_enqueue_style( 'estilos', get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0', 'all');
+}
+
+add_action( 'wp_enqueue_scripts','assets');
