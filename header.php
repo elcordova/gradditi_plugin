a215dcf0db9c8e0a9e5be13d1e76596bcec563b2
elcordova <erick.lenin.03@gmail.com>
2020-05-18 19:07:54 -0500

    agregando javascript al tema

--- header.php
+++ header.php
@@ -6,3 +6,12 @@
   <?php wp_head() ?>
 </head>
 <body>
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <img src="<?php echo get_template_directory_uri().'/assets/img/logo.png' ?>" alt="logo_gradditi">
+      </div>
+    </div>
+  </div>
+</header>
