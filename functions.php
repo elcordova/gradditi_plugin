bebd1d62fb106ecc13860f97fcf2c4adb8b0f0d5
elcordova <erick.lenin.03@gmail.com>
2020-05-18 19:52:57 -0500

    gennerando menú

--- functions.php
+++ functions.php
@@ -2,6 +2,12 @@
 function init_template() {
   add_theme_support('post-thumbnails');
   add_theme_support('title-tag');
+
+  register_nav_menus( 
+    array(
+      'top_menu' => 'Menú Principal'
+    )
+  );
 }
 
 add_action('after_setup_theme', 'init_template');
--- header.php
+++ header.php
@@ -8,9 +8,20 @@
 <body>
 <header>
   <div class="container">
-    <div class="row">
+    <div class="row align-items-center">
       <div class="col-4">
         <img src="<?php echo get_template_directory_uri().'/assets/img/logo.png' ?>" alt="logo_gradditi">
+      </div>
+      <div class="col-8">
+        <nav>
+          <?php wp_nav_menu( 
+            array(
+              'theme_location' => 'top_menu',
+              'menu_class' => 'menu-principal',
+              'container_class' => 'container_menu'
+            )
+           ) ?>
+        </nav>
       </div>
     </div>
   </div>
