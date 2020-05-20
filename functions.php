342c210f5ac4091b5d22ac53a789de620cadb5c5
elcordova <erick.lenin.03@gmail.com>
2020-05-20 06:40:48 -0500

    generando un custom post type

--- functions.php
+++ functions.php
@@ -39,3 +39,31 @@ function sidebar() {
 }
 
 add_action( 'widgets_init', 'sidebar');
+
+function productos_type(){
+
+    $labels = array(
+        'name' => 'Productos',
+        'singular_name' => 'Producto',
+        'menu_name' => 'Productos'
+    );
+
+    $args = array(
+        'label' => 'Productos',
+        'description' => 'Productos de platzi',
+        'labels' => $labels,
+        'supports' => array('title','editor','thumbnail','revision'),
+        'public' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-cart',
+        'can_export' => true,
+        'publicy_queryable' =>  true,
+        'rewrite'=>true,
+        'show_in_rest' => true
+    );
+
+    register_post_type('producto', $args);
+}
+
+add_action('init','productos_type');
--- single.php
+++ single.php
@@ -4,12 +4,12 @@
     while (have_posts()) {
       the_post();
       ?>
-      <h1 class='my-3'><?php the_title(); ?></h1>
+      <h1 class='my-5'><?php the_title(); ?></h1>
       <div class="row">
-        <div class="col-6">
+        <div class="col-4">
           <?php the_post_thumbnail('large'); ?>
         </div>
-        <div class="col-6">
+        <div class="col-8">
           <?php the_content(); ?>
         </div>
       </div>
