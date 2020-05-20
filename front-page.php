2aac178981ece3fff175faa0ebda56bb4559b9d1
elcordova <erick.lenin.03@gmail.com>
2020-05-20 08:26:18 -0500

    custom loop en página principal

--- front-page.php
+++ front-page.php
@@ -13,6 +13,40 @@
     }
   }
   ?>
+  <div class="lista-productos">
+    <h2 class="text-center">
+      PRODUCTOS
+    </h2>
+    <div class="row">
+    <?php 
+      $args = array(
+        'post_type' => 'producto',//corresponde con el nombre de registro
+        'post_per_page' => -1, // all default configuracion de administración
+        'order'=> 'ASC',//por defecto 'DESC',
+        'orderby'=> 'title' // modificando parámertro de búsqueda default 'date'
+      );
+      $productos= new WP_Query($args);
+
+      if ($productos->have_posts()) {
+        while ($productos->have_posts()) {
+          $productos->the_post();
+          ?>
+          <div class="col-4 my-5">
+            <figure>
+              <?php the_post_thumbnail('large');?>
+            </figure>
+            <h4 class="my-3 text-center"><a href="<?php the_permalink(); ?>">
+              <?php the_title(); ?>
+            </a></h4>
+          </div>
+          <?php
+        }
+      }
+
+    ?>
+    </div>
+  </div>
+
 </main>
 
 <?php get_footer();?>
