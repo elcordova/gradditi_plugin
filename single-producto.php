a3cd2eb182fea2b02dd1e156ab938a1621698617
elcordova <erick.lenin.03@gmail.com>
2020-05-26 11:17:52 -0500

    email

--- single-producto.php
+++ single-producto.php
@@ -5,12 +5,15 @@
       the_post();
       ?>
       <?php $taxonomy = get_the_terms( get_the_ID(), 'categoria-productos')?>
-      <h1 class='my-5'><?php the_title(); ?></h1>
+      <h1 class='row my-5'><?php the_title(); ?></h1>
       <div class="row">
-        <div class="col-4">
+        <div class="col-md-6 col-12">
           <?php the_post_thumbnail('large'); ?>
         </div>
-        <div class="col-8">
+        <div class="col-md-6 col-12">
+          <?php echo do_shortcode('[contact-form-7 id="47" title="Contact form 1"]') ?>
+        </div>
+        <div class="col-12">
           <?php the_content(); ?>
         </div>
       </div>
