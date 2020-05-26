cbf7919116c42fcf554692cf8e840682e61e7b4e
elcordova <erick.lenin.03@gmail.com>
2020-05-26 17:08:23 -0500

    end-point en wordpress

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -27,4 +27,34 @@
       }
     })
   })
+
+  $(document).ready(function(){
+    $.ajax({
+      url: pg.api_url+"novedades/3",
+      method: 'GET',
+      data: {
+        "action": "pgFiltroProductos",
+        "categoria": $(this).find(':selected').val()
+      },
+      beforeSend: function() {
+        $('#resultado-novedades').html('Cargando...');
+      },
+      success: function(data) {
+        let html ='';
+        data.forEach(item=> {
+          html += `<div class="col-4 my-3">
+          <figure>${item.imagen}</figure>
+          <h4 class="text-center my-2">
+          <a href="${item.link}"> ${item.titulo}</a>
+          </h4>
+          </div>`;
+        });
+        $('#resultado-novedades').html(html);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    })
+  })
+
 })(jQuery);
