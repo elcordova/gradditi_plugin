29fde10a4cbe82669acbedfa8a0f17220e8f4b51
elcordova <erick.lenin.03@gmail.com>
2020-05-27 12:18:32 -0500

    creacion y obtimizacion de bloque con gutenberg

---
+++ blocks/src/index.js
@@ -0,0 +1,29 @@
+import { registerBlockType } from "@wordpress/blocks";
+import { TextControl } from "@wordpress/components";
+
+registerBlockType('pg/basic', 
+  {
+    title: "Basic Block",
+    description: "Este es el primer bloque",
+    icon: "smiley",
+    category: "layout",
+    attributes: {
+      content: {
+        type: "string",
+        default: "Hello World"
+      }
+    },
+    edit: (props) => {
+      const { attributes: { content }, setAttributes, className, isSelected } = props;
+      const handlerOnChangeInput = (newContent) => {
+        setAttributes({content: newContent});
+      }
+      return <TextControl
+              label= "complete el campo"
+              value = {content}
+              onChange = {handlerOnChangeInput}
+            />
+    },
+    save: (props) => <h2>{props.attributes.content}</h2>,
+  }
+);
